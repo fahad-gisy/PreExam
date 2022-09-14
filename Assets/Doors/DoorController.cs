f3ba9eddc105c98b9d49844bfe24c3cce6caaa8f
abdullah <nm7.abs@gmail.com>
2022-09-14 22:55:47 +0300

    Merge branch 'main' of https://github.com/fahad-gisy/PreExam into DoorsAnimation
    # Conflicts:
    #	Assets/Scenes/SampleScene.unity

--- Assets/Doors/DoorController.cs
+++ Assets/Doors/DoorController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+   private bool _Key1;
    private void OnTriggerEnter(Collider other)
    {
       if (other.CompareTag("Door"))
@@ -13,6 +14,22 @@ public class DoorController : MonoBehaviour
          Animator anim = other.GetComponentInChildren<Animator>();
          anim.SetBool("OpenClose",true);
 
+      }
+
+      if (other.CompareTag("Key1"))
+      {
+         _Key1 = !_Key1;
+         Debug.Log(_Key1);
+         Destroy(other.gameObject);
+      }
+
+      if (other.CompareTag("LockedDoor1"))
+      {
+         if (_Key1)
+         {
+            Animator anim = other.GetComponentInChildren<Animator>();
+            anim.SetBool("Open",true);
+         }
       }
    }
 
