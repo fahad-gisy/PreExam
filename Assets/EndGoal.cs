14c3aa0d8e468f54211d5c8f09e7fec8671aa654
abdullah <nm7.abs@gmail.com>
2022-09-15 16:07:20 +0300

    end goal

--- Assets/EndGoal.cs
+++ Assets/EndGoal.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGoal : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
        //end goal trigger with player
+       SceneManager.LoadScene("WinMenu");
     }
 }
