438d5d72e636a7b5811ed0ddb48a60019010c686
abdullah <nm7.abs@gmail.com>
2022-09-15 10:05:38 +0300

    door puzzle

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+   private bool _key1;
    private void OnTriggerEnter(Collider other)
    {
       if (other.CompareTag("Door"))
@@ -13,6 +15,21 @@ public class DoorController : MonoBehaviour
          Animator anim = other.GetComponentInChildren<Animator>();
          anim.SetBool("OpenClose",true);
 
+      }
+
+      if (other.CompareTag("Key1"))
+      {
+         _key1 = !_key1;
+         Destroy(other.gameObject);
+      }
+
+      if (other.CompareTag("LockedDoor1"))
+      {
+         if (_key1)
+         {
+            Animator anim = other.GetComponentInChildren<Animator>();
+            anim.SetBool("Open",true);
+         }
       }
    }
 
